fb3456b4f3cbfb6d727661ce3be0189416b40276
unknown <kevinsalinas200408@gmail.com>
2024-11-18 00:23:21 +0100

    Primer commit

---
+++ app/src/main/java/com/example/lyloudine/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lyloudine;
+
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int tiempoCarga = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        }, tiempoCarga);
+
+    }
+}
